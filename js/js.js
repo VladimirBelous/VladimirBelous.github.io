b50999e59b9d347a28e7cb1ce55cd3955307a31e
VladimirBelous <39527818+VladimirBelous@users.noreply.github.com>
2018-09-06 21:52:11 +0300

    version 0.9

--- js/js.js
+++ js/js.js
@@ -1,8 +1,11 @@
+// БУРГЕР
 $('.menu__btn').on('click', function(e) {
   e.preventDefault;
   $(this).toggleClass('menu__btn__active')
 });
 
+
+// МЕНЮ
 $(document).ready(function(){
 			var touch = $('.menu__btn');
 		    var menu = $('.nav');
@@ -19,8 +22,10 @@ $(document).ready(function(){
 		    });
 		});
 
+// СЛАЙДЕР
  $('.sl').slick();
 
+// Форма обратной связи
  function AjaxFormRequest(result_id,formMain,url) {
                 jQuery.ajax({
                     url:     url,
@@ -41,3 +46,14 @@ $(document).ready(function(){
  				.removeAttr('checked')
  				.removeAttr('selected');
 } 
+
+// Плавная прокрутка
+$(document).ready(function(){
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+  var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+      return false; // выключаем стандартное действие
+    });
+});
