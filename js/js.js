9f509e508b3b7d5ad2c157d25fcbf23344527f25
VladimirBelous <39527818+VladimirBelous@users.noreply.github.com>
2018-09-18 00:43:26 +0300

    own_return

--- js/js.js
+++ js/js.js
@@ -1,11 +1,8 @@
-// БУРГЕР
 $('.menu__btn').on('click', function(e) {
   e.preventDefault;
   $(this).toggleClass('menu__btn__active')
 });
 
-
-// МЕНЮ
 $(document).ready(function(){
 			var touch = $('.menu__btn');
 		    var menu = $('.nav');
@@ -22,32 +19,12 @@ $(document).ready(function(){
 		    });
 		});
 
+
+
 // СЛАЙДЕР
  $('.sl').slick();
 
-// Форма обратной связи
-$(document).ready(function(){
-    $('#button_contacts').click(function(){
-        var form_name   = $('#form_name').val();
-        var form_email   = $('#form_email').val();
-        var form_message = $('#form_message').val();
-        $.ajax({
-            url: "post.php", 
-            type: "post", 
-            dataType: "json", 
-            data: { 
-                "form_name":   form_name,
-                "form_email":   form_email,
-                "form_message": form_message
-            },
-            success: function(data){
-                $('.messages').html(data.result); 
-            }
-        });
-    });
-});
-
-// Плавная прокрутка
+ // Плавная прокрутка
 $(document).ready(function(){
     $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
   var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
